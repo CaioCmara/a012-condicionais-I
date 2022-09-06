d7bf1bbb2f6268a0722b750a0a747dac14430b04
CaioCmara <caio1801@gmail.com>
2022-09-06 16:39:28 -0300

    exercicio resolvido

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,31 @@
-let nacionalidade //= adicione o prompt aqui
+ 
+let nacionalidade = prompt("Insira sua nacionalidade"); //= adicione o prompt aqui
+
+if (
+  nacionalidade === "brasileira" ||
+  nacionalidade === "argentina" ||
+  nacionalidade === "uruguaia" ||
+  nacionalidade === "chilena" ||
+  nacionalidade === "colombiana"
+) {
+  console.log("Sua nacionalidade é: ", nacionalidade);
+} else {
+  console.log("Nacionalidade não encontrada");
+}
+
+
+/*
+if (nacionalidade === "brasileira") {
+  console.log(nacionalidade);
+} else if (nacionalidade === "argentina") {
+  console.log(nacionalidade);
+} else if (nacionalidade === "uruguaia") {
+  console.log(nacionalidade);
+} else if (nacionalidade === "chilena") {
+  console.log(nacionalidade);
+} else if (nacionalidade === "colombiana") {
+  console.log(nacionalidade);
+} else {
+  console.log("Nacionalidade não encontrada")
+} 
+*/
